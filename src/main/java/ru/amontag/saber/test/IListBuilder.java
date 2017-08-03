a50520efb69c97f2530e7e75734f98881918c9ad
montag <montag@yandex-team.ru>
2017-08-03 11:24:42 +0300

    delete

--- src/main/java/ru/amontag/saber/test/IListBuilder.java
+++
@@ -1,7 +0,0 @@
-package ru.amontag.saber.test;
-
-public interface IListBuilder {
-    ListRand build();
-    void add(String data, Integer rndNodeIndex);
-    void add(String data);
-}
--- src/main/java/ru/amontag/saber/test/ListNode.java
+++
@@ -1,8 +0,0 @@
-package ru.amontag.saber.test;
-
-public class ListNode {
-    public ListNode prev;
-    public ListNode next;
-    public ListNode rand;
-    public String data;
-}
